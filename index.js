4fc4aac9d2a5b20cbd90aa759dbcc4ecd49bae7e
Antony Fagundez <jhon.fagundez@softtek.com>
2020-08-25 23:09:13 -0300

    Initial Project

---
+++ index.js
@@ -0,0 +1,20 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, function (req, res) {
+    fs.readFile(__dirname + req.url, function (err,data) {
+        if (err) {
+          res.writeHead(404);
+          res.end(JSON.stringify(err));
+          return;
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+    console.log('Listen in port 8000')
+}).listen(8000);
